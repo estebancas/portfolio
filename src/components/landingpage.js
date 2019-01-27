086dac61f8e23267e134a18b0f89d8a2168fdfc1
estebancas <estcascor94@hotmail.com>
2019-01-27 14:15:17 -0600

    change in about me section

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Grid, Cell } from 'react-mdl'
+import { Grid, Cell, Footer, FooterSection, FooterLinkList } from 'react-mdl'
 import logo from '../assets/imgs/myAvatar.png'
+import section from '../assets/imgs/section1.jpg'
 
 class Landing extends Component {
     render() {
@@ -34,28 +35,39 @@ class Landing extends Component {
                         </div>
                     </Cell>
                 </Grid>
-                <section className="aboutme" id="aboutme">
-                    <div>
-                        <h2 style={{textAlign: 'center'}}>
-                            About Me
-                        </h2>
-                        <hr />
-                        <p>
-                            Information system engineer with 2+ year experience as full stack developer. Prominent javaScript knowledge with ES6/7 standard, knowlegde in JS frontend frameworks such as Angular 2+, react, jQuery. Also experience with BackEnd applications, RESTful APIs using NodeJS, Express or sails.
-                            Also:
-                        </p>
-                        <ul>
-                            <li>
-                                <strong>Common libraries: </strong>
-                                Bluebird, body-parser, hash, Mongoose, lodash
-                            </li>
-                            <li>
-                                <strong>Common UI libraries: </strong>
-                                Bootstrap, ngx-Bootstrap, material-ui, angular-material, materialize, react-mdl
-                            </li>
-                        </ul>
+                <section className="section1" style={{ background: 'url(' + section + ') no-repeat' }}>
+                    <div className="overlay">
+                        <section className="aboutme" id="aboutme">
+                            <div>
+                                <h2 style={{ textAlign: 'center' }}>
+                                    About Me
+                                </h2>
+                                <hr />
+                                <p>
+                                    Information system engineer with 2+ year experience as full stack developer. Prominent javaScript knowledge with ES6/7 standard, knowlegde in JS frontend frameworks such as Angular 2+, react, jQuery. Also experience with BackEnd applications, RESTful APIs using NodeJS, Express or sails.
+                                    Also:
+                                </p>
+                                <ul>
+                                    <li>
+                                        <strong>Common libraries: </strong>
+                                        <br />
+                                        Bluebird, body-parser, hash, Mongoose, lodash
+                                    </li>
+                                    <li>
+                                        <strong>Common UI libraries: </strong>
+                                        <br />
+                                        Bootstrap, ngx-Bootstrap, material-ui, angular-material, materialize, react-mdl
+                                    </li>
+                                </ul>
+                            </div>
+                        </section>
                     </div>
                 </section>
+                <Footer size="mini">
+                    <FooterSection type="left" logo="Esteban Castro, 2019">
+
+                    </FooterSection>
+                </Footer>
             </div>
         )
     }
