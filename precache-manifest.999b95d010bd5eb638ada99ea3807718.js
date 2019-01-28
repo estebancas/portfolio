e4fc8576ca1dc05ec7ae9d74374a940d8ffaeb38
estebancas <estcascor94@hotmail.com>
2019-01-27 23:55:19 -0600

    Updates

---
+++ precache-manifest.999b95d010bd5eb638ada99ea3807718.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c4adbbe9987ff2342b9f8b2543cf90b",
+    "url": "/portfolio/static/media/section1.9c4adbbe.jpg"
+  },
+  {
+    "revision": "ff2229edb884023df0e0d84ad2bbd730",
+    "url": "/portfolio/static/media/myAvatar.ff2229ed.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5a28f669113553694f1e",
+    "url": "/portfolio/static/js/main.5a28f669.chunk.js"
+  },
+  {
+    "revision": "17446c8d2348f5ee709e",
+    "url": "/portfolio/static/js/1.17446c8d.chunk.js"
+  },
+  {
+    "revision": "5a28f669113553694f1e",
+    "url": "/portfolio/static/css/main.66cdf409.chunk.css"
+  },
+  {
+    "revision": "17446c8d2348f5ee709e",
+    "url": "/portfolio/static/css/1.bd5c88ad.chunk.css"
+  },
+  {
+    "revision": "55bbba18cdd45df0c43b4ca9f60455eb",
+    "url": "/portfolio/index.html"
+  }
+];
