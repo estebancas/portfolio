3bbd1cf6e87b4e74e9d60755d93dcee98b0a179c
estebancas <estcascor94@hotmail.com>
2019-01-28 00:16:19 -0600

    host changes

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, HashRouter } from 'react-router-dom'
 
 import LandingPage from './landingpage'
 import AboutMe from './aboutme'
@@ -8,7 +8,7 @@ import Resume from './resume'
 import Contact from './contact'
 
 const Main = () => (
-
+    <HashRouter>
         <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/aboutme" component={AboutMe} />
@@ -16,6 +16,7 @@ const Main = () => (
             <Route path="/projects" component={Projects} />
             <Route path="/resume" component={Resume} />
         </Switch>
+    </HashRouter>
 )
 
 export default Main;
